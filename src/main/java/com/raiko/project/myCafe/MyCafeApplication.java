5155c7c42fcf4c472602711679cafafe5756533d
ALIAKSEI RAIKO <alexeyraiko644@gmail.com>
2023-06-16 11:46:05 +0300

    Initial commit

---
+++ src/main/java/com/raiko/project/myCafe/MyCafeApplication.java
@@ -0,0 +1,13 @@
+package com.raiko.project.myCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raiko/project/myCafe/MyCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raiko.project.myCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
